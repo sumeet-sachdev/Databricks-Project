0961d1549c337681f5489fded3a914dc6ad1455b
Nithin Sai Sudulakunta <nithin.sudulakunta@incedoinc.com>
2023-08-14 15:40:44 -0400

    convert input file to csv and put in o/p s3 bucket

--- POC-2/lambda_tocsv.py
+++ POC-2/lambda_tocsv.py
@@ -15,26 +15,39 @@ def lambda_handler(event, context):
         for record in event['Records']:
             s3bucket = record['s3']['bucket']['name']
             print("Bucket Name is {}".format(s3bucket))
+            
+            s3outputbucket = "bkt-poc2-output"
+            
             s3key = record['s3']['object']['key']
             print("Bucket Key name is {}".format(s3key))
 
-            parquet_object = s3_client.get_object(Bucket=s3bucket, Key=s3key)
-            parquet_data = parquet_object['Body'].read()
+            format_object = s3_client.get_object(Bucket=s3bucket, Key=s3key)
+            format_data = format_object['Body'].read()
 
             # Create a BytesIO file-like object from the raw Parquet data
-            parquet_file = BytesIO(parquet_data)
+            format_file = BytesIO(format_data)
+            
+            if s3key.split('.')[-1].endswith('parquet'):
 
-            df = pd.read_parquet(parquet_file)
+                df = pd.read_parquet(format_file)
+                
+            if s3key.split('.')[-1].endswith('json'):
 
+                df = pd.read_json(format_file) 
+                
+            if s3key.split('.')[-1].endswith('csv'):
+
+                df = pd.read_csv(format_file)
+                
             csv_data = df.to_csv(index=False)
 
             file_name = s3key.split('/')[-1]
             file_name_without_extension = file_name.split('.')[0]
-            to_key = f"csv_output_folder/{file_name_without_extension}.csv"
+            to_key = f"{file_name_without_extension}.csv"
 
-            s3_client.put_object(Body=csv_data.encode(), Bucket=s3bucket, Key=to_key)
+            s3_client.put_object(Body=csv_data.encode(), Bucket=s3outputbucket, Key=to_key)
 
-            print(f"Converted Parquet file to CSV: s3://{s3bucket}/{s3key} -> s3://{s3bucket}/{to_key}")
+            print(f"Converted  input file to CSV: s3://{s3bucket}/{s3key} -> s3://{s3outputbucket}/{to_key}")
 
     except Exception as e:
         print("An error occurred: {}".format(e))
