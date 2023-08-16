f061081864deb8c029940a19dd65b4fe628301e6
tan-incedo <taniya.chauhan1@incedoinc.com>
2023-08-16 12:02:18 -0700

    proceed

--- config.ini.py
+++ config.ini.py
@@ -12,6 +12,18 @@ config_object["OutputFile"] = {
     "Output File": "product_data.parquet",
 
 }
+config_object["AK"] = {
+    "AK": "AKIAW5IZRWOY5GUDLZRL",
+
+}
+config_object["SAK"] = {
+    "SAK": "4WJxnNEzUHxg/fhOSVBi/U",
+
+}
+config_object["path"] = {
+    "path": "C:/Users/rohan.bangera/Downloads/data/product_data.parquet",
+
+}
 
 
 #Write the above sections to config.ini file
